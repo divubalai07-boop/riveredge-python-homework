533b0535d5f40d1b69bac8d40772bed63b995f7d
divyesh <divubalai07@gmail.com>
2026-07-19 17:17:03 +0530

    Complete Problem 3

---
+++ p03.py
@@ -0,0 +1,13 @@
+password = input("Enter password: ")
+
+score = 0
+
+# now i can not solve further
+
+
+if score <= 1:
+    print("Score:", score, "/4 - Weak")
+elif score <= 3:
+    print("Score:", score, "/4 - Okay")
+else:
+    print("Score:", score, "/4 - Strong")
