676289b445324cbf8e54a9050c330c2fd3293aa4
divyesh <divubalai07@gmail.com>
2026-07-22 00:09:29 +0530

    updated

--- p04.py
+++ p04.py
@@ -4,7 +4,8 @@ raw = ['  divyesh BALAI', 'rajat  ', 'Divyesh Balai', 'RAJAT', 'khushi shah', ' 
 clean = []
 
 for name in raw:
-    name = " ".join(name.split())   # word split and join to remove extra spaces
+    word = name.split()
+    name = " ".join(word)   # word split and join to remove extra spaces
     name = name.title()             # show in proper case title ,capatilize first letter of each word
 
     if name not in clean:
